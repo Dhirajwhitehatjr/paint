5130744a5d6a89705678340a05fbd525da7af992
Dhirajwhitehatjr <83785808+Dhirajwhitehatjr@users.noreply.github.com>
2021-09-10 22:25:56 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 var last_position_of_x, last_position_of_y;
 
     canvas = document.getElementById('myCanvas');
-    ctx = canvas.getContext("3d");
+    ctx = canvas.getContext("2d");
     
     color = "black";
     width_of_line = 2;
